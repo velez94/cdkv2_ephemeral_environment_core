db779592f92a15263d1a5a71e21ae11f2c747a01
alejandro.velez <labvel@VelPro>
2023-08-27 17:42:12 -0500

    Initial commit

---
+++ test/cdkv2_ephemeral_environment_core.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdkv2EphemeralEnvironmentCore from '../lib/cdkv2_ephemeral_environment_core-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdkv2_ephemeral_environment_core-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdkv2EphemeralEnvironmentCore.Cdkv2EphemeralEnvironmentCoreStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
